0a744cb9dfdaf940d1ac9ce45a19c0c4b904e224
wzxinchen <1101385391@qq.com>
2020-01-05 13:06:32 +0800

    图片上传

---
+++ src/Blazui.Markdown/IconHandlers/ImageHandler.cs
@@ -0,0 +1,44 @@
+﻿using Blazui.Component;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazui.Markdown.IconHandlers
+{
+    public class ImageHandler : IIconHandler
+    {
+        private readonly IJSRuntime jSRuntime;
+        private readonly DialogService dialogService;
+
+        public ImageHandler(IJSRuntime jSRuntime, DialogService dialogService)
+        {
+            this.jSRuntime = jSRuntime;
+            this.dialogService = dialogService;
+        }
+
+        public async Task HandleAsync(ElementReference textarea)
+        {
+            var imageName = await jSRuntime.InvokeAsync<string>("getSelection", textarea);
+            var imageModel = new ImageModel
+            {
+                Alt = imageName,
+                Title = imageName
+            };
+            var parameters = new Dictionary<string, object>();
+            parameters.Add(nameof(Image.Image), imageModel);
+            parameters.Add(nameof(Image.UploadUrl), "http://localhost");
+            var result = await dialogService.ShowDialogAsync<Image, ImageModel>("插入图片", parameters);
+            imageModel = result.Result;
+            var title = imageModel.Title;
+            if (!string.IsNullOrWhiteSpace(title))
+            {
+                title = $"\"{title}\"";
+            }
+            var image = $"![{imageModel.Alt}]({imageModel.Urls} {title})";
+            await jSRuntime.InvokeVoidAsync("replaceSelection", textarea, image);
+        }
+    }
+}
---
+++ src/Blazui.Markdown/ImageBase.cs
@@ -0,0 +1,32 @@
+﻿using Blazui.Component;
+using Blazui.Component.Form;
+using Blazui.Markdown.IconHandlers;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazui.Markdown
+{
+    public class ImageBase : BComponentBase
+    {
+        internal protected BForm form;
+
+        [Parameter]
+        public ImageModel Image { get; set; }
+
+        [Parameter]
+        public string UploadUrl { get; set; }
+        [Inject]
+        internal DialogService DialogService { get; set; }
+
+        internal protected void Submit()
+        {
+            if (!form.IsValid())
+            {
+                return;
+            }
+            _ = DialogService.CloseDialogAsync(this, form.GetValue<ImageModel>());
+        }
+    }
+}
